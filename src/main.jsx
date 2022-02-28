4fdba5399169cae173450de17c7301507be0cd8c
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-02-28 09:53:56 -0400

    config framekor for style componet

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { ChakraProvider } from "@chakra-ui/react";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ChakraProvider>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-)
+  </ChakraProvider>,
+  document.getElementById("root")
+);
